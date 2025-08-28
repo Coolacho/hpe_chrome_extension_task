c1ebb4ab90311adb7ec9ad5b6c3acce70608447d
Angel Angelov <sofiaangel35@gmail.com>
2025-08-28 17:17:15 +0300

    Added timestamp marking

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -6,6 +6,7 @@ export default function Popup() {
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
     const [imgSrc, setImgSrc] = useState("");
+    const [timeStamp, setTimeStamp] = useState("");
 
     function onClick() {
         setIsError(false);
@@ -15,6 +16,7 @@ export default function Popup() {
             .then(res => {
                 setIsLoading(false)
                 setImgSrc(res[0].url)
+                setTimeStamp(new Date().toTimeString())
             })
             .catch(() => {
                 setIsLoading(false)
@@ -30,6 +32,7 @@ export default function Popup() {
                 {isError && (<p>Sorry, there was an error generating your image, please try again.</p>)}
                 {imgSrc !== "" && (<img alt="Generated image" src={imgSrc}/>)}
             </div>
+            {timeStamp !== "" && (<div>Last generation: {timeStamp}</div>)}
             <Button onClick={onClick}/>
         </div>
     )
