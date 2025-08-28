4c42d201d17bcde092894d1872caa896b20f0b26
Angel Angelov <sofiaangel35@gmail.com>
2025-08-28 14:49:57 +0300

    Added error handling and img placeholder

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -3,16 +3,28 @@ import Button from "./Button.jsx";
 
 export default function Popup() {
     const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [imgSrc, setImgSrc] = useState("");
 
     function onClick() {
+        setIsError(false);
+        setImgSrc("");
         setIsLoading(true);
-        setTimeout(() => {setIsLoading(false)}, 3000)
+        setTimeout(() => {
+            setIsLoading(false)
+            //setIsError(true)
+            //setImgSrc("https://cdn2.thecatapi.com/images/MTk1NjM4MA.jpg")
+        }, 3000)
     }
 
     return (
         <div>
             <h2>Random image generator extension</h2>
-            {isLoading && (<p>Loading...</p>)}
+            <div>
+                {isLoading && (<p>Loading...</p>)}
+                {isError && (<p>Sorry, there was an error generating your image, please try again.</p>)}
+                {imgSrc !== "" && (<img alt="Generated image" src={imgSrc}/>)}
+            </div>
             <Button onClick={onClick}/>
         </div>
     )
